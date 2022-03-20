7d023b72890639f6f4592cf5f85711b57c08b3af
Kritik09 <kritikmanral1@gmail.com>
2022-03-20 13:28:45 +0530

    MORE RESPONSIVENESS

--- src/components/RespNav/NavLinks.jsx
+++ src/components/RespNav/NavLinks.jsx
@@ -4,22 +4,22 @@ export const NavLinks = () => {
   return (
     <div className={classes.respnav__links}>
         <div className={classes.respnav__link}>
-            <a href="#home" className={classes.respnav__item}><h1>HOME</h1></a>
+            <a href="#home" className={classes.respnav__item}><h2>HOME</h2></a>
         </div>
         <div className={classes.respnav__link}>
-            <a href="#about" className={classes.respnav__item}><h1>ABOUT</h1></a>
+            <a href="#about" className={classes.respnav__item}><h2>ABOUT</h2></a>
         </div>
         <div className={classes.respnav__link}>
-            <a href="#achievements" className={classes.respnav__item}><h1>ACHIEVEMENTS</h1></a>
+            <a href="#achievements" className={classes.respnav__item}><h2>ACHIEVEMENTS</h2></a>
         </div>
         <div className={classes.respnav__link}>
-            <a href="#skills" className={classes.respnav__item}><h1>SKILLS</h1></a>
+            <a href="#skills" className={classes.respnav__item}><h2>SKILLS</h2></a>
         </div>
         <div className={classes.respnav__link}>
-            <a href="#projects" className={classes.respnav__item}><h1>PROJECTS</h1></a>
+            <a href="#projects" className={classes.respnav__item}><h2>PROJECTS</h2></a>
         </div>
         <div className={classes.respnav__link}>
-            <a href="#contact" className={classes.respnav__item}><h1>CONTACT</h1></a>
+            <a href="#contact" className={classes.respnav__item}><h2>CONTACT</h2></a>
         </div>
     </div>
   )
