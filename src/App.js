91474f266783f062fda8f174f20bd6d235c39d1e
Kritik09 <kritikmanral1@gmail.com>
2022-03-25 15:49:46 +0530

    NEW

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React,{useEffect} from "react";
 import { About } from "./components/About/About";
 import { Achievements } from "./components/Achievements/Achievements";
 import { Contact } from "./components/Contact/Contact";
@@ -7,31 +6,7 @@ import { Header } from "./components/Header/Header";
 import { Projects } from "./components/Projects/Projects";
 import { RespNav } from "./components/RespNav/RespNav";
 import { Skills } from "./components/Skills/Skills";
-import scrollreveal from "scrollreveal";
 function App() {
-  useEffect(()=>{
-    const sr=scrollreveal({
-      origin:"top",
-      distance:"80px",
-      duration:2000,
-      reset:true,
-    });
-    sr.reveal(
-      `
-      #home,
-      #about,
-      #achievements,
-      #skills,
-      #projects,
-      #contact,
-      #footer
-      `,
-      {
-        opacity:0,
-        interval:300,
-      }
-    );
-  },[]);
   return (
     <div>
         <Header/>
